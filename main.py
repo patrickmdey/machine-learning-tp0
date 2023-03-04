a34f159541c3dcc27017bede518a3a3e3a63bb07
patrickmdey <patrickmdey@hotmail.com>
2023-03-04 17:04:13 -0300

    Fix: bad data interpretation

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ def main():
     df = pd.read_csv("./Datos1.csv", usecols=['Grasas_sat','Alcohol','Calorías','Sexo'])
     method = "median"
 
-    df["Calorías"] = df["Calorías"].str.replace(",", ".").astype(float)
-    df["Sexo"] = df["Sexo"].replace("F","0").replace("M","1").astype(int)
 
+    df["Calorías"] = df["Calorías"].str.replace(",", "").astype(int)
+    print(df["Calorías"].head())    
+
+    df["Sexo"] = df["Sexo"].replace("F","0").replace("M","1").astype(int)
 
     df = replace_missing_values(df, "Alcohol", 999.99, method)
     df = replace_missing_values(df, "Grasas_sat", 999.99, method)
-    df = replace_missing_values(df, "Calorías", 800.0, method)
 
     boxplot_column(df, "Alcohol", method)
     boxplot_column(df, "Grasas_sat", method)
     boxplot_column(df, "Calorías", method)
     boxplot_column(df, "Sexo", method)
+
+    
 
 
 if __name__ == "__main__":
