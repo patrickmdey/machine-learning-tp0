718ed0cb8bbac81da39f392d36987abf9c9b9f09
patrickmdey <patrickmdey@hotmail.com>
2023-03-04 18:04:02 -0300

    Feat: scatter and cov analysis
    Co-authored-by: matiaslombardi <mflombardi2000@gmail.com>
    Co-authored-by: igvazquez
    <igvazquez@itba.edu.ar>

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import numpy as np
-from utils import replace_missing_values, boxplot_column
+from utils import replace_missing_values, boxplot_column, cov_analysis, histogram_column, categorize_calories_col, scatter_category
 
 def main():
     df = pd.read_csv("./Datos1.csv", usecols=['Grasas_sat','Alcohol','Calorías','Sexo'])
@@ -8,18 +7,29 @@ def main():
 
 
     df["Calorías"] = df["Calorías"].str.replace(",", "").astype(int)
-    print(df["Calorías"].head())    
 
     df["Sexo"] = df["Sexo"].replace("F","0").replace("M","1").astype(int)
 
     df = replace_missing_values(df, "Alcohol", 999.99, method)
     df = replace_missing_values(df, "Grasas_sat", 999.99, method)
 
+    # boxplot de todas las variables
     boxplot_column(df, "Alcohol", method)
     boxplot_column(df, "Grasas_sat", method)
     boxplot_column(df, "Calorías", method)
-    boxplot_column(df, "Sexo", method)
+    histogram_column(df, "Sexo")
 
+    # analisis de covarianza
+    input_df = df.loc[:,df.columns != "Sexo"]
+
+    normalized_df=(input_df-input_df.mean())/input_df.std()
+
+    normalized_df["Sexo"] = df["Sexo"]
+    cov_analysis(normalized_df)
+
+    # categorizando calorias
+    categorized_df = categorize_calories_col(df)
+    scatter_category(categorized_df, "Alcohol", "Categorías")
     
 
 
--- utils.py
+++ utils.py
@@ -14,18 +14,27 @@ def replace_missing_values(df, column, value, method="mean"):
     # print()
     if (method == "mean"):
         # print("Replacing for mean...")
-        mean_df.loc[mean_df[column] >= value, column] = df_mean
-        # mean_df[column].replace(value, df_mean, inplace=True)
+        # mean_df.loc[mean_df[column] >= value, column] = df_mean
+        mean_df[column].replace(value, df_mean, inplace=True)
         # print("New " + column +" mean:", mean_df[column].mean())
         # print("New " + column +" median:", mean_df[column].median())
         return mean_df
     elif (method == "median"):
         # print("Replacing for median...")
-        median_df.loc[median_df[column] >= value, column] = df_median
-        # median_df[column].replace(value, df_median, inplace=True)
+        # median_df.loc[median_df[column] >= value, column] = df_median
+        median_df[column].replace(value, df_median, inplace=True)
         # print("New " + column +" mean:", median_df[column].mean())
         # print("New " + column +" median:", median_df[column].median())
         return median_df
+    
+def cov_analysis(df):
+    print(df.groupby("Sexo").cov())
+
+def histogram_column(df, column_name):
+    plt.clf()
+    df.hist(column=column_name)
+    plt.savefig("./out/"+column_name + "_histogram.png")
+
 
 def boxplot_column(df, column_name, method=""):
     plt.clf()
@@ -35,3 +44,32 @@ def boxplot_column(df, column_name, method=""):
     boxplot.set_ylabel("Valor")
     str = column_name + "_with_" + method if method != "" else column_name
     boxplot.figure.savefig("./out/"+str + "_boxplot.png")
+
+# Define a function to categorize the values based on the conditions
+def categorize(val):
+    if val <= 1100:
+        return 'CAT1'
+    elif val <= 1700:
+        return 'CAT2'
+    else:
+        return 'CAT3'
+
+def categorize_calories_col(df):
+    df_copy = df.copy()
+
+    df_copy['Categorías'] = df['Calorías'].apply(categorize)
+
+    # df_copy = df.assign(Category=lambda x: "CATE1" if x <=["Calorías"] 1100 else ("CATE2" if x["Calorías"] <= 1700 else "CATE3"))
+
+    return df_copy
+
+
+def scatter_category(df, x, y):
+    plt.clf()
+
+    scatter = df.plot.scatter(x, y)
+
+    scatter.set_title(x + " vs " + y)
+    scatter.set_xlabel(x)
+    scatter.set_ylabel("Calorías")
+    scatter.figure.savefig("./out/"+x + "_vs_" + y + "_scatter.png")
